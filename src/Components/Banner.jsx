0c6bc843190b274277ad21dc59e109fd991f856b
Pratham <prathams@krishworks.com>
2023-07-23 12:28:20 +0530

    banner section skelton ready

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <div className="banner-content">
+        <h1>Product Tagline</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum,
+          voluptatem quod! Animi, consectetur atque incidunt excepturi
+          voluptatibus hic saepe recusandae dignissimos quis obcaecati!
+        </p>
+      </div>
+      <div className="banner-image">
+        <img src="https://www.placehold.co/300x400" alt="Product" />
+      </div>
+    </div>
+  );
+}
