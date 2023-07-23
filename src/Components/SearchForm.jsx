22b437e8af7b85d5fd8fdc66f475c319ccdc82bb
Pratham <prathams@krishworks.com>
2023-07-23 14:07:57 +0530

    search form made

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SearchForm() {
-  return <div>SearchForm</div>;
+  const [filterType, setFilterType] = useState("default");
+  const [searchText, setSearchText] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilterType(event.target.value);
+  };
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Call your API here using filterType and searchText
+    // Reset the form if needed
+    setSearchText("");
+  };
+
+  return (
+    <form className="search-form" onSubmit={handleSubmit}>
+      <select value={filterType} onChange={handleFilterChange}>
+        <option value="default">Select Filter</option>
+        <option value="filter1">Filter 1</option>
+        <option value="filter2">Filter 2</option>
+        {/* Add more filter options if needed */}
+      </select>
+      <input
+        type="text"
+        value={searchText}
+        onChange={handleSearchTextChange}
+        placeholder="enter text"
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
 }
